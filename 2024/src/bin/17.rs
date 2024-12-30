3940fbc0e528ea27d6099f056c059275c095235d
Hitoshi Yoshida <github@peria.jp>
2024-12-31 00:30:37 +0900

    Update for 17-2. Not solved yet.

--- 2024/src/bin/17.rs
+++ 2024/src/bin/17.rs
@@ -15,24 +15,29 @@ fn input() -> Computer {
 }
 
 fn solve1(computer: &Computer) -> String {
-    computer.run()
+    let outputs = computer.run();
+    outputs
+        .iter()
+        .map(|x| x.to_string())
+        .collect::<Vec<_>>()
+        .join(",")
 }
 
-fn solve2(computer: &Computer) -> String {
-    "".to_string()
+fn solve2(computer: &Computer) -> u64 {
+    computer.solve()
 }
 
 #[derive(Debug, Clone)]
 struct Computer {
-    reg_a: i64,
-    reg_b: i64,
-    reg_c: i64,
+    reg_a: u64,
+    reg_b: u64,
+    reg_c: u64,
     program: Vec<u8>,
 }
 
 impl Computer {
     const ADV: u8 = 0; // Ra <= Ra / 2^COMBO
-    const BXL: u8 = 1; // Rb <= Rb & 1
+    const BXL: u8 = 1; // Rb <= Rb & operand
     const BST: u8 = 2; // Rb <= COMBO % 8
     const JNZ: u8 = 3; // if Ra != 0 then PC <= operand
     const BXC: u8 = 4; // Rb <= Rb ^ Rc
@@ -40,13 +45,27 @@ impl Computer {
     const BDV: u8 = 6; // Rb <= Ra / 2^COMBO
     const CDV: u8 = 7; // Rc <= Ra / 2^COMBO
 
-    fn run(&self) -> String {
+    fn run(&self) -> Vec<u8> {
         let mut outputs = Vec::new();
         let mut reg_a = self.reg_a;
         let mut reg_b = self.reg_b;
         let mut reg_c = self.reg_c;
         let mut pc = 0;
         loop {
+            // eprintln!("Ra: {}", reg_a);
+            // eprintln!("Rb: {}", reg_b);
+            // eprintln!("Rc: {}", reg_c);
+            // eprintln!(
+            //     "{}",
+            //     self.program
+            //         .iter()
+            //         .map(|x| x.to_string())
+            //         .collect::<Vec<_>>()
+            //         .join(",")
+            // );
+            // eprint!("{:width$}", "", width = pc * 2);
+            // eprintln!("^");
+
             if pc >= self.program.len() {
                 break;
             }
@@ -70,7 +89,7 @@ impl Computer {
                 }
                 Self::BXL => {
                     // Rb <= Rb & 1
-                    reg_b &= 1;
+                    reg_b ^= operand as u64;
                 }
                 Self::BST => {
                     // Rb <= COMBO % 8
@@ -89,7 +108,7 @@ impl Computer {
                 }
                 Self::OUT => {
                     // Print(COMBO % 8)
-                    outputs.push(combo() % 8);
+                    outputs.push(combo() as u8 % 8);
                 }
                 Self::BDV => {
                     // Rb <= Ra / 2^COMBO
@@ -111,17 +130,35 @@ impl Computer {
             }
         }
 
-        let values: Vec<_> = outputs.iter().map(|x| x.to_string()).collect();
-        values.join(",")
+        outputs
+    }
+
+    fn solve(&self) -> u64 {
+        const START: u64 = 0o3004700000000000;
+        let mut computer = self.clone();
+        let mut ra = START;
+        let mut max_len = 0;
+        loop {
+            computer.reg_a = ra;
+            let output = computer.run();
+            if output == computer.program {
+                return ra;
+            }
+            if ra < START + 10u64 {
+                eprintln!("{:o} > {:?} {:?}", ra, &output, &self.program);
+                max_len = output.len();
+            }
+            ra += 1;
+        }
     }
 }
 
 impl From<&str> for Computer {
     fn from(value: &str) -> Self {
         let lines: Vec<_> = value.trim().lines().collect();
-        let reg_a = sscanf::sscanf!(lines[0], "Register A: {i64}").ok().unwrap();
-        let reg_b = sscanf::sscanf!(lines[1], "Register B: {i64}").ok().unwrap();
-        let reg_c = sscanf::sscanf!(lines[2], "Register C: {i64}").ok().unwrap();
+        let reg_a = sscanf::sscanf!(lines[0], "Register A: {u64}").ok().unwrap();
+        let reg_b = sscanf::sscanf!(lines[1], "Register B: {u64}").ok().unwrap();
+        let reg_c = sscanf::sscanf!(lines[2], "Register C: {u64}").ok().unwrap();
         let code = lines[4].split(' ').nth(1).unwrap();
         let program = code.split(',').map(|x| x.parse().unwrap()).collect();
 
@@ -140,18 +177,33 @@ mod test {
     use crate::solve2;
     use crate::Computer;
 
-    const SAMPLE_INPUT: &str = "Register A: 729
+    const SAMPLE_INPUT_1: &str = "Register A: 729
 Register B: 0
 Register C: 0
 
 Program: 0,1,5,4,3,0
 ";
 
+    const SAMPLE_INPUT_2: &str = "Register A: 2024
+Register B: 0
+Register C: 0
+
+Program: 0,3,5,4,3,0
+";
+
     #[test]
     fn test_solve1() {
-        let computer = Computer::from(SAMPLE_INPUT);
+        let computer = Computer::from(SAMPLE_INPUT_1);
         let actual = solve1(&computer);
         let expect = "4,6,3,5,6,3,5,2,1,0";
         assert_eq!(&actual as &str, expect);
+    }
+
+    #[test]
+    fn test_solve2() {
+        let computer = Computer::from(SAMPLE_INPUT_2);
+        let actual = solve2(&computer);
+        let expect = 117440;
+        assert_eq!(actual, expect);
     }
 }
