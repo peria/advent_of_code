224107f7af8919a90aece351650b3f41ec3fa741
Hitoshi Yoshida <github@peria.jp>
2025-12-07 15:18:29 +0900

    WIP 06-2

--- 2025/src/bin/06.rs
+++ 2025/src/bin/06.rs
@@ -1,7 +1,5 @@
 use std::io::BufRead;
 
-use itertools::multizip;
-
 fn main() {
     let data = get_input();
     let answer1 = solve1(&data);
@@ -10,16 +8,11 @@ fn main() {
     println!("Part 2: {}", answer2);
 }
 
-fn get_input() -> Vec<Vec<String>> {
+fn get_input() -> Vec<Vec<char>> {
     std::io::stdin()
         .lock()
         .lines()
-        .map(|line| {
-            line.unwrap()
-                .split_ascii_whitespace()
-                .map(|x| x.to_string())
-                .collect()
-        })
+        .map(|line| line.unwrap().chars().collect())
         .collect()
 }
 
@@ -28,20 +21,23 @@ struct Value {
     operator: char,
 }
 
-fn solve1(data: &Vec<Vec<String>>) -> i64 {
-    let ops = data.last().unwrap();
-    let nums = &data[..(data.len() - 1)];
+fn solve1(data: &Vec<Vec<char>>) -> i64 {
+    let ops = data.last().unwrap().iter().collect::<String>();
+    let ops = ops.split_ascii_whitespace();
     let mut values: Vec<Value> = ops
-        .iter()
         .map(|op| Value {
             nums: Vec::new(),
             operator: op.chars().nth(0).unwrap(),
         })
         .collect();
     for line in data.iter().take(data.len() - 1) {
-        values.iter_mut().zip(line.iter()).for_each(|(v, n)| {
-            v.nums.push(n.parse::<i64>().unwrap());
-        });
+        let nums = line.iter().collect::<String>();
+        values
+            .iter_mut()
+            .zip(nums.split_ascii_whitespace())
+            .for_each(|(v, n)| {
+                v.nums.push(n.parse::<i64>().unwrap());
+            });
     }
 
     values
@@ -54,25 +50,52 @@ fn solve1(data: &Vec<Vec<String>>) -> i64 {
         .sum()
 }
 
-fn solve2(data: &Vec<Vec<String>>) -> i64 {
-    0
+fn solve2(data: &Vec<Vec<char>>) -> i64 {
+    let n = data.len() - 1;
+    let (num_lines, operation_line) = (&data[..n], &data[n]);
+
+    let mut total = 0i64;
+    let mut nums = vec![0i64; n];
+    for i in (0..(data[0].len())).rev() {
+        for (l, val) in num_lines.iter().zip(nums.iter_mut()) {
+            let c = l[i];
+            if c == ' ' {
+                continue;
+            }
+            *val = *val * 10 + (c as u8 - b'0') as i64;
+        }
+        if operation_line[i] == ' ' {
+            continue;
+        }
+
+        let operator = operation_line[i];
+        let line_total: i64 = match operator {
+            '+' => nums.iter().sum(),
+            '*' => nums.iter().product(),
+            _ => 0,
+        };
+        total += line_total;
+        for val in nums.iter_mut() {
+            *val = 0;
+        }
+    }
+    total
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
 
-    const SAMPLE_INPUT: &str = r"123 328  51 64 
- 45 64  387 23 
+    const SAMPLE_INPUT: &str = r"123 328  51 64
+ 45 64  387 23
   6 98  215 314
 *   +   *   + ";
 
     #[test]
     fn test_solve1() {
         let input: Vec<Vec<_>> = SAMPLE_INPUT
-            .trim()
             .split('\n')
-            .map(|s| s.split_ascii_whitespace().map(|x| x.to_string()).collect())
+            .map(|s| s.chars().collect())
             .collect();
         assert_eq!(solve1(&input), 4277556);
     }
@@ -80,10 +103,9 @@ mod test {
     #[test]
     fn test_solve2() {
         let input: Vec<Vec<_>> = SAMPLE_INPUT
-            .trim()
             .split('\n')
-            .map(|s| s.split_ascii_whitespace().map(|x| x.to_string()).collect())
+            .map(|s| s.chars().collect())
             .collect();
-        assert_eq!(solve2(&input), 13);
+        assert_eq!(solve2(&input), 3263827);
     }
 }
